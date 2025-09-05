b5b7fbaba53c6d82277a58b1b05b7437dc72d324
unknown <you@example.com>
2025-09-05 08:38:22 -0700

    add url

--- src/components/common/http.jsx
+++ src/components/common/http.jsx
@@ -1,5 +1,5 @@
-export const apiUrl = "http://localhost:8000/api";
-export const fileUrl = "http://localhost:8000/";
+export const apiUrl = "https://web-production-d56e.up.railway.app/api";
+export const fileUrl = "https://web-production-d56e.up.railway.app/";
 export const token = () => {
   const adminInfo = localStorage.getItem("adminInfo");
   const data = JSON.parse(adminInfo);
