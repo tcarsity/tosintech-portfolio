76058eb41762c8d07efec754f0a700cd920510fb
unknown <you@example.com>
2026-01-08 12:36:46 +0100

    url error fix

--- src/components/common/http.jsx
+++ src/components/common/http.jsx
@@ -1,5 +1,5 @@
-export const apiUrl = "https://web-production-d56e.up.railway.app/api";
-export const fileUrl = "https://web-production-d56e.up.railway.app/";
+export const apiUrl = "https://tosintech-website-backend.onrender.com/api";
+export const fileUrl = "https://tosintech-website-backend.onrender.com/";
 export const token = () => {
   const adminInfo = localStorage.getItem("adminInfo");
   const data = JSON.parse(adminInfo);
